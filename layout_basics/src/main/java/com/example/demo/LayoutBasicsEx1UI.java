866d020a1a0dd75cfb3e4c8a6223973ae8b2e5a3
JeanBilheux <bilheuxjm@ornl.gov>
2019-05-30 11:41:52 -0400

    Added original solution to example 1.

--- layout_basics/src/main/java/com/example/demo/LayoutBasicsEx1UI.java
+++ layout_basics/src/main/java/com/example/demo/LayoutBasicsEx1UI.java
@@ -19,6 +19,26 @@ public class LayoutBasicsEx1UI extends UI {
 	}
 
 	
+//	private void setupLayout() {
+//
+//		HorizontalLayout root = new HorizontalLayout();
+//		
+//		Button button1 = new Button("Button 1");
+//		Button button2 = new Button("Button 2");
+//		Button button3 = new Button("Button 3");
+//
+//		HorizontalLayout rightLayout = new HorizontalLayout();
+//		Button button4 = new Button("Button 4");
+//		rightLayout.addComponent(button4);
+//		rightLayout.setComponentAlignment(button4, Alignment.MIDDLE_RIGHT);
+//		
+//		root.addComponents(button1, button2, button3);
+//		root.addComponentsAndExpand(rightLayout);
+//		
+//		setContent(root);
+//		
+//	}
+	
 	private void setupLayout() {
 
 		HorizontalLayout root = new HorizontalLayout();
@@ -26,18 +46,16 @@ public class LayoutBasicsEx1UI extends UI {
 		Button button1 = new Button("Button 1");
 		Button button2 = new Button("Button 2");
 		Button button3 = new Button("Button 3");
-
-		HorizontalLayout rightLayout = new HorizontalLayout();
 		Button button4 = new Button("Button 4");
-		rightLayout.addComponent(button4);
-		rightLayout.setComponentAlignment(button4, Alignment.MIDDLE_RIGHT);
 		
-		root.addComponents(button1, button2, button3);
-		root.addComponentsAndExpand(rightLayout);
-		
+		root.addComponents(button1, button2, button3, button4);
+		root.setExpandRatio(button4,  1);
+		root.setComponentAlignment(button4,  Alignment.TOP_RIGHT);
+		root.setWidth("100%");
+		root.setSpacing(true);
 		
 		setContent(root);
 		
 	}
-	
+
 }
