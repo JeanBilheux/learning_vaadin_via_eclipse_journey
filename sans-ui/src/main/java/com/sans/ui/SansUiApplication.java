370161d0f4c7fac98eea56a308061ebd1ba4eb26
JeanBilheux <bilheuxjm@ornl.gov>
2019-05-31 16:15:19 -0400

    making sure I'm sync to keep working on this over the week end

---
+++ sans-ui/src/main/java/com/sans/ui/SansUiApplication.java
@@ -0,0 +1,13 @@
+package com.sans.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SansUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SansUiApplication.class, args);
+	}
+
+}
